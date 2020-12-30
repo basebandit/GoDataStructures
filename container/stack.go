74a819dc9e51bcf2dcac9fd3779ea8d243a4e050
basebandit <evansonmwangi83@gmail.com>
2020-12-30 23:50:11 +0300

    implement linked implementation of the stack ADT

--- container/stack.go
+++ container/stack.go
@@ -59,3 +59,60 @@ func (s *ArrayStack) Size() int {
 func (s *ArrayStack) Clear() {
 	s.store = make([]interface{}, 0, 10)
 }
+
+//node an aggregate variable with data and link(reference) fields. This forms our
+//backing linked data structure for the stack ADT linked implementation.
+type node struct {
+	item interface{}
+	next *node
+}
+
+//LinkedStack is an implementation of the stack ADT using a linked data structure (a node).
+type LinkedStack struct {
+	topPtr *node //singly-linked list of values
+	count  int   //how many elements are present
+}
+
+//Pop removes the top most element from the stack and returns it.
+func (s *LinkedStack) Pop() (interface{}, error) {
+	if s.count == 0 {
+		return nil, ErrPopEmpty
+	}
+
+	result := s.topPtr.item
+	s.topPtr = s.topPtr.next
+	s.count--
+
+	return result, nil
+}
+
+//Push adds a new element on to the top of the stack.
+func (s *LinkedStack) Push(e interface{}) {
+	s.topPtr.item = &node{item: e}
+	s.count++
+}
+
+//Top returns the top of the stack
+func (s *LinkedStack) Top() (interface{}, error) {
+	if s.count == 0 {
+		return nil, ErrTopEmpty
+	}
+
+	return s.topPtr.item, nil
+}
+
+//Empty returns the state of the stack, true if is empty false otherwise.
+func (s *LinkedStack) Empty() bool {
+	return s.count > 0
+}
+
+//Size returns the number of elements held by the stack.
+func (s *LinkedStack) Size() int {
+	return s.count
+}
+
+//Clear empties the stack
+func (s *LinkedStack) Clear() {
+	s.count = 0
+	s.topPtr = nil
+}
