b5d32ea01a51cb5842dd7029189a98d0c6a61e1b
Capricorn <evansonmwangi83@gmail.com>
2023-01-12 11:42:00 +0300

    implmentation of selection sort

---
+++ selectionsort/ssort.go
@@ -0,0 +1,17 @@
+// Copyright  Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package selectionsort
+
+// selectionSort searches for the smallest element and swaps it with the current index,
+// and it repeats with each iteration increasing the index.It does this inplace. No need to allocate a new array(memory)
+func selectionSort(nums []int) {
+	length := len(nums)
+	for i := 0; i < length-1; i++ {
+		for j := i + 1; j < length; j++ {
+			if nums[j] < nums[i] {
+				nums[j], nums[i] = nums[i], nums[j]
+			}
+		}
+	}
+}
---
+++ selectionsort/ssort_test.go
@@ -0,0 +1,18 @@
+// Copyright  Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package selectionsort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSelectionSort(t *testing.T) {
+	arr := []int{3, 5, 4, 1, 2}
+	want := []int{1, 2, 3, 4, 5}
+	selectionSort(arr)
+	if !reflect.DeepEqual(arr, want) {
+		t.Fatalf("Expected %v got %v", want, arr)
+	}
+}
