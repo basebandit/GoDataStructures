486536b580fcd93bed04d5236e7a7737953512de
Capricorn <evansonmwangi83@gmail.com>
2023-01-12 11:39:30 +0300

    implement linkedlist

---
+++ linkedlist/list.go
@@ -0,0 +1,49 @@
+// Copyright  Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package linkedlist
+
+import (
+	"errors"
+)
+
+type node struct {
+	elem any
+	next *node
+}
+
+type LinkedList struct {
+	header *node
+	count  int
+}
+
+func (l *LinkedList) Size() int {
+	return l.count
+}
+func (l *LinkedList) Empty() bool {
+	return l.count == 0
+}
+func (l *LinkedList) Clear() {
+	l.count = 0
+	l.header = nil
+}
+func (l *LinkedList) Push(e any) {
+	l.header = &node{e, l.header}
+	l.count++
+}
+func (l *LinkedList) Pop() (any, error) {
+	if l.count == 0 {
+		return nil, errors.New("list is empty")
+	}
+	result := l.header.elem
+	l.header = l.header.next
+	l.count--
+	return result, nil
+}
+
+func (l *LinkedList) Top() (any, error) {
+	if l.count == 0 {
+		return nil, errors.New("list is empty")
+	}
+	return l.header.elem, nil
+}
---
+++ linkedlist/list_test.go
@@ -0,0 +1,35 @@
+// Copyright  Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package linkedlist
+
+import (
+	"testing"
+)
+
+func TestPush(t *testing.T) {
+	list := LinkedList{}
+
+	type testCases struct {
+		want  any
+		input any
+	}
+
+	tests := []testCases{
+		{1, 1},
+		{2, 2},
+		{3, 3},
+	}
+
+	for _, tt := range tests {
+		list.Push(tt.input)
+		got, err := list.Top()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got != tt.want {
+			t.Fatalf("Expected %d Got %d", got, tt.want)
+		}
+	}
+
+}
