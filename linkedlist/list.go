de7607259dafbb2709db61bc2577d4e026e733bb
Capricorn <evansonmwangi83@gmail.com>
2023-01-13 15:09:45 +0300

    implement a singly linkedlist

--- linkedlist/list.go
+++ linkedlist/list.go
@@ -1,7 +1,7 @@
 package linkedlist
 
 import (
-	"errors"
+	"fmt"
 )
 
 type node struct {
@@ -9,38 +9,102 @@ type node struct {
 	next *node
 }
 
-type LinkedList struct {
-	header *node
-	count  int
+type SinglyLinkedList struct {
+	head *node
+	tail *node
+	size int
 }
 
-func (l *LinkedList) Size() int {
-	return l.count
-}
-func (l *LinkedList) Empty() bool {
-	return l.count == 0
-}
-func (l *LinkedList) Clear() {
-	l.count = 0
-	l.header = nil
-}
-func (l *LinkedList) Push(e any) {
-	l.header = &node{e, l.header}
-	l.count++
-}
-func (l *LinkedList) Pop() (any, error) {
-	if l.count == 0 {
-		return nil, errors.New("list is empty")
+// Prepend inserts a new node at the beginning
+func (s *SinglyLinkedList) Prepend(data any) {
+	if s.head == nil {
+		s.head = &node{elem: data}
+		s.tail = s.head
+	} else {
+		n := &node{elem: data}
+		n.next = s.head
+		s.head = n
 	}
-	result := l.header.elem
-	l.header = l.header.next
-	l.count--
-	return result, nil
+	s.size++
 }
 
-func (l *LinkedList) Top() (any, error) {
-	if l.count == 0 {
-		return nil, errors.New("list is empty")
+// Append inserts a new node at the end
+func (s *SinglyLinkedList) Append(data any) {
+	if s.head == nil {
+		s.head = &node{elem: data}
+		s.tail = s.head
+	} else {
+		n := &node{elem: data}
+		s.tail.next = n
+		s.tail = n
 	}
-	return l.header.elem, nil
+	s.size++
+}
+
+// Get retrieves the value of the index-th node in the linked list. Returns nil if node not found
+// takes O(n) time.
+func (s *SinglyLinkedList) Get(index int) any {
+	if index < 0 || s.head == nil || index >= s.size {
+		return nil
+	}
+
+	current := s.head
+	for i := 0; i < index; i++ {
+		current = current.next
+	}
+
+	if current == nil {
+		return nil
+	}
+	
+	return current.elem
+}
+
+// AddAt inserts a new node at the index-th of the linked list
+// Takes O(n) time
+func (s *SinglyLinkedList) AddAt(index int, data any) error {
+	if index < 0 || index > s.size {
+		return fmt.Errorf("index %d is out of bounds", index)
+	}
+
+	if index == s.size {
+		s.Append(data)
+	} else {
+		current := s.head
+		for i := 0; i < index-1; i++ {
+			current = current.next
+		}
+		n := &node{elem: data}
+		n.next = current.next
+		current.next = n
+	}
+	s.size++
+	return nil
+}
+
+// DeleteAt deletes the index-th node of the linkedlist
+// takesO(n) time
+func (s *SinglyLinkedList) DeleteAt(index int) error {
+	if index < 0 || index >= s.size {
+		return fmt.Errorf("index %d is out of bounds", index)
+	}
+	if index == 0 {
+		s.head = s.head.next
+		if s.head == nil {
+			s.tail = nil
+		}
+	} else {
+		current := s.head
+		for i := 0; i < index-1; i++ {
+			current = current.next
+		}
+		if current.next.next == nil {
+			current.next = nil
+			s.tail = current
+		} else {
+			current.next = current.next.next
+		}
+	}
+	s.size--
+	return nil
 }
--- linkedlist/list_test.go
+++ linkedlist/list_test.go
@@ -4,29 +4,57 @@ import (
 	"testing"
 )
 
-func TestPush(t *testing.T) {
-	list := LinkedList{}
-
-	type testCases struct {
-		want  any
-		input any
+func TestSinglyLinkedList(t *testing.T) {
+	list := SinglyLinkedList{}
+	list.Prepend(1)
+	want := 1
+	got := list.Get(0)
+	if got != want {
+		t.Fatalf("Expected %d got %d", want, got)
+	}
+	list.Prepend(2)
+	want = 2
+	got = list.Get(0)
+	if got != want {
+		t.Fatalf("Expected %d got %d", want, got)
+	}
+	want = 1
+	got = list.Get(1)
+	if got != want {
+		t.Fatalf("Expected %d got %d", want, got)
 	}
 
-	tests := []testCases{
-		{1, 1},
-		{2, 2},
-		{3, 3},
+	err := list.AddAt(2, 3)
+	if err != nil {
+		t.Fatalf("error occured: %v", err)
+	}
+	want = 3
+	got = list.Get(2)
+	if got != want {
+		t.Fatalf("Expected %d got %d", want, got)
+	}
+	err = list.AddAt(3, 4)
+	if err != nil {
+		t.Fatalf("error occured: %v", err)
+	}
+	want = 4
+	got = list.Get(3)
+	if got != want {
+		t.Fatalf("Expected %d got %d", want, got)
+	}
+	err = list.DeleteAt(3)
+	if err != nil {
+		t.Fatalf("error occured: %v", err)
 	}
 
-	for _, tt := range tests {
-		list.Push(tt.input)
-		got, err := list.Top()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if got != tt.want {
-			t.Fatalf("Expected %d Got %d", got, tt.want)
-		}
+	got = list.Get(3)
+	if got != nil {
+		t.Fatalf("Expected nil got %v", got)
 	}
 
+	want = 3
+	got = list.Get(2)
+	if got != want {
+		t.Fatalf("Expected %v got %v", want, got)
+	}
 }
